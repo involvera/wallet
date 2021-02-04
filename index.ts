71eada8934b639eb3bd79c8107976e14121fbfb1
fantasim <suiol@protonmail.com>
2021-02-04 21:54:48 +0200

    .

---
+++ index.ts
@@ -0,0 +1,13 @@
+
+import * as bip39 from 'bip39'
+import * as bip32 from 'bip32'
+import * as bs58check from 'bs58check'
+
+const main = async () => {
+    const mnemonic = bip39.generateMnemonic()
+    const seed = await bip39.mnemonicToSeed(mnemonic, "")
+    const p = bip32.fromSeed(seed)
+    console.log(bs58check.encode(p.publicKey as Buffer))
+}
+
+main()
