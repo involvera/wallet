b8593e25f2518eca1a0107ce5e75fa1a1fb60b8e
fantasim <siuol@gmx.com>
2021-05-31 07:27:38 +0300

    index

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as Wallet from './src/wallet'
 import * as Transaction from './src/transaction'
-import * as Script from './src/script'
+import * as Script from './src/scriptV2'
 import * as Util from './src/util/wallet'
 import * as Constant from './src/constant'
 import Config from './src/config'
