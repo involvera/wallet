83c47a38ac16ad3cd73df13b203d28090c21e44e
fantasim <siuol@gmx.com>
2021-07-11 16:07:57 +0300

    fix totalVotePower

--- src/wallet/puts.ts
+++ src/wallet/puts.ts
@@ -214,8 +214,10 @@ export class UnserializedPutList extends Collection {
         }
 
         const votePowerPercent = (lh: number): number => {
-            const total = Number(BigInt(totalVotePower() as any) / BigInt(10))
-
+            const total_vp = totalVotePower()
+            if (total_vp === BigInt(0))
+                return 0
+            const total = Number(BigInt(totalVotePower as any) / BigInt(10))
             if (lh >= CYCLE_IN_LUGH){
                 const max = CYCLE_IN_LUGH * (LUGH_AMOUNT / 10)
                 return total / max
