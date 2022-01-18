5f39f264fbef83f3639b02df46f67f0b1d516cd6
fantasim <siuol@gmx.com>
2022-01-18 08:50:00 +0200

    uservote createdAtPretty method

--- src/off-chain/proposal/user-vote.ts
+++ src/off-chain/proposal/user-vote.ts
@@ -1,5 +1,6 @@
 import { Model } from "acey";
 import { IUserVote } from 'community-coin-types'
+import moment from 'moment'
 
 export const DEFAULT_STATE: IUserVote = {
     has_approved: false,
@@ -17,7 +18,21 @@ export class UserVoteModel extends Model {
     get = () => {
         return {
             hasApproved: (): boolean => this.state.has_approved,
-            voteLH: (): number => this.state.vote_lh
+            voteLH: (): number => this.state.vote_lh,
+            createdAtPretty: (currentLH: number): string =>{
+                const vlh = this.get().voteLH()
+                const diffLH = currentLH - vlh
+                if (diffLH < 3)
+                    return 'newly'
+                if (diffLH < 6)
+                    return 'recently'
+                let d = new Date().getTime()
+                d -= (Math.ceil(diffLH / 3) * 24 * 60 * 60 * 1000)
+                return moment(d).fromNow()
+            }
         }
     }
+
+
+
 }
